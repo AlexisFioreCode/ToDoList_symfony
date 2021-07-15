146c98b6e310c0a721bf6b895b75daf431981f99
AlexisFioreCode <alexis.fiorentino.pro@gmail.com>
2021-07-15 17:29:35 +0200

    done update for tasks and changed state

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+/* let task = document.getElementsByClassName('task');
+let state = document.getElementsByClassName('state');
+state = state.innerHTML;
+console.log(state);
+console.log(task);
+if (task.innerText("Terminé") !== -1) {
+        this.classlitst.add("done")
+        
+} 
+ */
+
+
+
+
+
